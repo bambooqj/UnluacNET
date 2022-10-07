4be3cfc2b359fa9fa6f884d219484519d220db56
berkaybayar <berkaybyr@icloud.com>
2022-10-07 18:34:03 +0300

    Added folder support, can use folder paths as input and output. Removed nolog param stuff

--- Test/Unluac/Program.cs
+++ Test/Unluac/Program.cs
@@ -13,11 +13,20 @@ namespace Unluac
     {
         static string version = "1.2.2";
 
+        static void PrintError(string message, int exitCode)
+        {
+            Console.Error.WriteLine("  error: {0}", message);
+            if (exitCode > 0)
+            {
+                Console.ReadLine();
+                Environment.Exit(exitCode);
+            }
+        }
         static void PrintError(string message)
         {
             Console.Error.WriteLine("  error: {0}", message);
-        }
 
+        }
         static void PrintError(string message, params object[] args)
         {
             PrintError(String.Format(message, args));
@@ -26,7 +35,8 @@ namespace Unluac
         static void PrintUsage(bool error)
         {
             var output = (error) ? Console.Error : Console.Out;
-            output.WriteLine("  usage: unluac <inFile> [:-nolog] <:outFile>");
+            output.WriteLine("  usage: unluac <inFile> <:outFile>");
+            output.WriteLine("  usage: unluac <inFolder> <:outFolder>");
         }
 
         static void PrintVersion(bool error)
@@ -44,109 +54,113 @@ namespace Unluac
                 return header.Function.Parse(fs, header);
             }
         }
-
-        static void Main(string[] args)
+        /// <summary>
+        /// Decompiles a singe .luac file and creates output to give output param
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="output"></param>
+        static void SaveSingleLua(string input, string output)
         {
-            if (args.Length == 0)
+            LFunction lMain = null;
+
+            try
+            {
+                lMain = FileToFunction(input);
+            }
+            catch (Exception e)
             {
                 PrintVersion(true);
-                PrintError("no input file provided");
-                PrintUsage(true);
+                PrintError(e.Message, 1);
+            }
 
-                Environment.Exit(1);
+            var d = new Decompiler(lMain);
+            d.Decompile();
+            var writeLog = true;
+            try
+            {
+                using (var writer = new StreamWriter(output))
+                {
+                    d.Print(new Output(writer));
+                    writer.Flush();
+                    if (writeLog)
+                        Console.WriteLine("successfully decompiled to '{0}'", output);
+                    return;
+                }
+            }
+            catch (Exception e)
+            {
+                PrintVersion(true);
+                PrintError(e.Message, 2);
+            }
+            d.Print();
+            if (System.Diagnostics.Debugger.IsAttached)
+                Console.ReadKey();
+        }
+        /// <summary>
+        /// Decompiles a directory of .luac files and creates output to give output param, it will also work with a single file
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="output"></param>
+        static void SaveLuaCommon(string input, string output)
+        {
+            if (Directory.Exists(input))
+            {
+                //Folder and all LUAC inside
+                var files = Directory.GetFiles(input, "*.luac", SearchOption.AllDirectories);
+                if (files.Length == 0)
+                {
+                    PrintError("No .luac files found in " + input, 3);
+                    return;
+                }
+                if (!Directory.Exists(output)) Directory.CreateDirectory(output);
+                foreach (var file in files)
+                {
+                    try
+                    {
+                        var parsed = file.Replace(input, "");
+                        var filename = Path.GetFileNameWithoutExtension(file);
+                        var parentDir = parsed.Replace(filename + ".luac", "");
+                        var dirToCreate = output + parentDir;
+                        if (!Directory.Exists(dirToCreate)) Directory.CreateDirectory(dirToCreate);
+                        var outputPath = dirToCreate + filename + ".lua";
+                        SaveSingleLua(file, outputPath);
+                    }
+                    catch (Exception ex)
+                    {
+                        PrintError(ex.Message + "\tFile: " + file);
+                    }
+                }
+                Console.WriteLine("Done!");
+                Console.ReadLine();
+                Environment.Exit(0);
+            }
+            else if (!File.Exists(input))
+            {
+                PrintVersion(true);
+                PrintUsage(true);
+                PrintError("input file does not exist", 1); //kills the app
+            }
+            SaveSingleLua(input, output); //File to File
+            Console.ReadLine();
+            Environment.Exit(0);
+        }
+        static void Main(string[] args)
+        {
+
+            if (args.Length != 2)
+            {
+                PrintVersion(true);
+                PrintUsage(true);
+                PrintError("missing args", 1);
             }
             else
             {
-                var fn = args[0];
-                LFunction lMain = null;
-
                 //Console.BufferHeight = 2500;
-
-                /* Error Levels
-                 * 
-                 * 0 - Successfully decompiled file
-                 * 1 - An error occurred while processing the input file
-                 * 2 - The specified output file could not be created
-                 */
-
-                try
-                {
-                    lMain = FileToFunction(fn);
-                }
-                catch (Exception e)
-                {
-                    PrintVersion(true);
-                    PrintError(e.Message);
-
-                    Environment.Exit(1);
-                }
-
-                var d = new Decompiler(lMain);
-
-                d.Decompile();
-
-                var writeLog = true;
-
-                if (args.Length > 1)
-                {
-                    // skip first arg
-                    for (int i = 1; i < args.Length; i++)
-                    {
-                        var arg = args[i];
-
-                        if (arg.StartsWith("-"))
-                        {
-                            switch (arg.ToLower().TrimStart('-'))
-                            {
-                            case "nolog":
-                                writeLog = false;
-                                continue;
-                            default:
-                                // just ignore it
-                                if (writeLog)
-                                    Console.WriteLine("unknown argument '{0}'", arg);
-                                continue;
-                            }
-                        }
-                        else
-                        {
-                            /** PROGRAM MUST TERMINATE ONCE FINISHED **/
-                            var filename = arg;
-
-                            try
-                            {
-                                using (var writer = new StreamWriter(filename))
-                                {
-                                    d.Print(new Output(writer));
-
-                                    writer.Flush();
-
-                                    if (writeLog)
-                                        Console.WriteLine("successfully decompiled to '{0}'", filename);
-
-                                    Environment.Exit(0);
-                                }
-                            }
-                            catch (Exception e)
-                            {
-                                PrintVersion(true);
-                                PrintError(e.Message);
-
-                                Environment.Exit(2);
-                            }
-                            /** PROGRAM MUST BE TERMINATED BY THIS POINT **/
-                        }
-                    }
-                }
-
-                // no output file was specified, spit out to console
-                d.Print();
-
-                if (System.Diagnostics.Debugger.IsAttached)
-                    Console.ReadKey();
-
-                Environment.Exit(0);
+                var input = args[0];
+                var output = args[1];
+                SaveLuaCommon(input, output);
             }
         }
     }
 }
+
